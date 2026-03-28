jellysquid3/sodium

Use alternative workaround for NVIDIA drivers
The NVIDIA driver enables a driver feature called
"Threaded Optimizations" when it finds Minecraft,
which causes severe performance issues and sometimes
even crashes.

Newer versions of the driver seem to use a slightly
different heuristic which our workaround doesn't
address.

So, instead, use an alternative method that enables
GL_DEBUG_OUTPUT_SYNCHRONOUS. This seems to reliably
disable the functionality *even if* the user has
configured it otherwise in their driver settings.

Additionally, on Windows, we now always indicate to
the driver that Minecraft is running, so that users
with hybrid graphics don't see regressed performance.

common/src/workarounds/java/net/caffeinemc/mods/sodium/client/compatibility/workarounds/nvidia/NvidiaWorkarounds.java
@@ -1,49 +1,154 @@
 package net.caffeinemc.mods.sodium.client.compatibility.workarounds.nvidia;
 
+import net.caffeinemc.mods.sodium.client.compatibility.environment.GlContextInfo;
 import net.caffeinemc.mods.sodium.client.compatibility.environment.OsUtils;
+import net.caffeinemc.mods.sodium.client.compatibility.environment.OsUtils.OperatingSystem;
+import net.caffeinemc.mods.sodium.client.compatibility.environment.probe.GraphicsAdapterProbe;
+import net.caffeinemc.mods.sodium.client.compatibility.environment.probe.GraphicsAdapterVendor;
+import net.caffeinemc.mods.sodium.client.compatibility.workarounds.Workarounds;
 import net.caffeinemc.mods.sodium.client.platform.unix.Libc;
-import net.caffeinemc.mods.sodium.client.platform.windows.api.Kernel32;
 import net.caffeinemc.mods.sodium.client.platform.windows.WindowsCommandLine;
+import net.caffeinemc.mods.sodium.client.platform.windows.WindowsFileVersion;
+import net.caffeinemc.mods.sodium.client.platform.windows.api.d3dkmt.D3DKMT;
+import org.jetbrains.annotations.Nullable;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL32C;
+import org.lwjgl.opengl.KHRDebug;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Locale;
 
 public class NvidiaWorkarounds {
     private static final Logger LOGGER = LoggerFactory.getLogger("Sodium-NvidiaWorkarounds");
 
-    public static void install() {
-        LOGGER.warn("Applying workaround: Prevent the NVIDIA OpenGL driver from using broken optimizations (NVIDIA_THREADED_OPTIMIZATIONS)");
+    public static boolean isUsingNvidiaGraphicsCard() {
+        return GraphicsAdapterProbe.getAdapters()
+                .stream()
+                .anyMatch(adapter -> adapter.vendor() == GraphicsAdapterVendor.NVIDIA);
+    }
 
-        try {
-            switch (OsUtils.getOs()) {
-                case WIN -> {
-                    // The NVIDIA drivers rely on parsing the command line arguments to detect Minecraft. If we destroy those,
-                    // then it shouldn't be able to detect us anymore.
-                    WindowsCommandLine.setCommandLine("net.caffeinemc.sodium");
+    // https://github.com/CaffeineMC/sodium/issues/1486
+    // The way which NVIDIA tries to detect the Minecraft process could not be circumvented until fairly recently
+    // So we require that an up-to-date graphics driver is installed so that our workarounds can disable the Threaded
+    // Optimizations driver hack.
+    public static @Nullable WindowsFileVersion findNvidiaDriverMatchingBug1486() {
+        // The Linux driver has two separate branches which have overlapping version numbers, despite also having
+        // different feature sets. As a result, we can't reliably determine which Linux drivers are broken...
+        if (OsUtils.getOs() != OperatingSystem.WIN) {
+            return null;
+        }
 
-                    // Ensures that Minecraft will run on the dedicated GPU, since the drivers can no longer detect it
-                    Kernel32.setEnvironmentVariable("SHIM_MCCOMPAT", "0x800000001");
-                }
-                case LINUX -> {
-                    // Unlike Windows, we don't need to hide ourselves from the driver. We can just request that
-                    // it not use threaded optimizations instead.
-                    Libc.setEnvironmentVariable("__GL_THREADED_OPTIMIZATIONS", "0");
+        for (var adapter : GraphicsAdapterProbe.getAdapters()) {
+            if (adapter.vendor() != GraphicsAdapterVendor.NVIDIA) {
+                continue;
+            }
+
+            if (adapter instanceof D3DKMT.WDDMAdapterInfo wddmAdapterInfo) {
+                var driverVersion = wddmAdapterInfo.openglIcdVersion();
+
+                if (driverVersion.z() == 15) { // Only match 5XX.XX drivers
+                    // Broken in x.y.15.2647 (526.47)
+                    // Fixed in x.y.15.3623 (536.23)
+                    if (driverVersion.w() >= 2647 && driverVersion.w() < 3623) {
+                        return driverVersion;
+                    }
                 }
             }
-        } catch (Throwable t) {
-            LOGGER.error("Failure while applying workarounds", t);
+        }
 
-            LOGGER.error("READ ME! The workarounds for the NVIDIA Graphics Driver did not apply correctly!");
-            LOGGER.error("READ ME! You are very likely going to run into unexplained crashes and severe performance issues!");
-            LOGGER.error("READ ME! Please see this issue for more information: https://github.com/CaffeineMC/sodium/issues/1816");
+        return null;
+    }
+
+    public static void applyEnvironmentChanges() {
+        // We can't know if the OpenGL context will actually be initialized using the NVIDIA ICD, but we need to
+        // modify the process environment *now* otherwise the driver will initialize with bad settings. For non-NVIDIA
+        // drivers, these workarounds are not likely to cause issues.
+        if (!isUsingNvidiaGraphicsCard()) {
+            return;
+        }
+
+        LOGGER.warn("Modifying process environment to apply workarounds for the NVIDIA graphics driver...");
+
+        try {
+            if (OsUtils.getOs() == OperatingSystem.WIN) {
+                applyEnvironmentChanges$Windows();
+            } else if (OsUtils.getOs() == OperatingSystem.LINUX) {
+                applyEnvironmentChanges$Linux();
+            }
+        } catch (Throwable t) {
+            LOGGER.error("Failed to modify the process environment", t);
+            logWarning();
         }
     }
 
-    public static void uninstall() {
-        switch (OsUtils.getOs()) {
-            case WIN -> {
-                WindowsCommandLine.resetCommandLine();
-            }
-            case LINUX -> { }
+
+    private static void applyEnvironmentChanges$Windows() {
+        // The NVIDIA drivers rely on parsing the command line arguments to detect Minecraft. We need to
+        // make sure that it detects the game so that *some* important optimizations are applied. Later,
+        // we will try to enable GL_DEBUG_OUTPUT_SYNCHRONOUS so that "Threaded Optimizations" cannot
+        // be enabled.
+        WindowsCommandLine.setCommandLine("net.caffeinemc.sodium / net.minecraft.client.main.Main /");
+    }
+
+    private static void applyEnvironmentChanges$Linux() {
+        // Unlike Windows, we can just request that it not use threaded optimizations instead.
+        Libc.setEnvironmentVariable("__GL_THREADED_OPTIMIZATIONS", "0");
+    }
+
+    public static void undoEnvironmentChanges() {
+        if (OsUtils.getOs() == OperatingSystem.WIN) {
+            undoEnvironmentChanges$Windows();
         }
+    }
+
+    private static void undoEnvironmentChanges$Windows() {
+        WindowsCommandLine.resetCommandLine();
+    }
+
+    public static void applyContextChanges(GlContextInfo driver) {
+        var normalizedVendorName = driver.vendor()
+                .toLowerCase(Locale.ROOT);
+
+        // The context may not have been initialized with the NVIDIA ICD, even if we think there is an NVIDIA
+        // graphics adapter in use. Because enabling these workarounds have the potential to severely hurt performance
+        // on other drivers, make sure we exit now.
+        if (!normalizedVendorName.startsWith("nvidia")) {
+            return;
+        }
+
+        LOGGER.warn("Modifying OpenGL context to apply workarounds for the NVIDIA graphics driver...");
+
+        if (Workarounds.isWorkaroundEnabled(Workarounds.Reference.NVIDIA_THREADED_OPTIMIZATIONS_BROKEN)) {
+            if (OsUtils.getOs() == OperatingSystem.WIN) {
+                applyContextChanges$Windows();
+            }
+        }
+    }
+
+    private static void applyContextChanges$Windows() {
+        // On Windows, the NVIDIA drivers do not have any environment variable to control whether
+        // "Threaded Optimizations" are enabled. But we can enable the "GL_DEBUG_OUTPUT_SYNCHRONOUS" option to
+        // achieve the same effect.
+        var capabilities = GL.getCapabilities();
+
+        if (capabilities.GL_KHR_debug) {
+            LOGGER.warn("Using GL_DEBUG_OUTPUT_SYNCHRONOUS to force the NVIDIA driver to disable threaded" +
+                    "command submission");
+            GL32C.glEnable(KHRDebug.GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        } else {
+            LOGGER.error("GL_KHR_debug does not appear to be supported on this system, unable to " +
+                    "disable threaded command submission for NVIDIA graphics driver! You are likely to " +
+                    "experience severe performance issues and frequent crashes.");
+            logWarning();
+        }
+    }
+
+    private static void logWarning() {
+        LOGGER.error("READ ME! The workarounds for the NVIDIA Graphics Driver did not apply correctly!");
+        LOGGER.error("READ ME! You are very likely going to run into unexplained crashes and severe performance issues.");
+        LOGGER.error("READ ME! More information about what went wrong can be found above this message.");
+        LOGGER.error("READ ME! Please help us understand why this problem occurred by opening a bug report on our issue tracker:");
+        LOGGER.error("READ ME!   https://github.com/CaffeineMC/sodium/issues");
     }
 }

PrestonGardner5081/sodium_fork

common/src/workarounds/java/net/caffeinemc/mods/sodium/client/compatibility/workarounds/nvidia/NvidiaWorkarounds.java
@@ -1,49 +1,157 @@
 package net.caffeinemc.mods.sodium.client.compatibility.workarounds.nvidia;
 
+import net.caffeinemc.mods.sodium.client.compatibility.environment.GlContextInfo;
 import net.caffeinemc.mods.sodium.client.compatibility.environment.OsUtils;
+import net.caffeinemc.mods.sodium.client.compatibility.environment.OsUtils.OperatingSystem;
+import net.caffeinemc.mods.sodium.client.compatibility.environment.probe.GraphicsAdapterProbe;
+import net.caffeinemc.mods.sodium.client.compatibility.environment.probe.GraphicsAdapterVendor;
+import net.caffeinemc.mods.sodium.client.compatibility.workarounds.Workarounds;
 import net.caffeinemc.mods.sodium.client.platform.unix.Libc;
-import net.caffeinemc.mods.sodium.client.platform.windows.api.Kernel32;
 import net.caffeinemc.mods.sodium.client.platform.windows.WindowsCommandLine;
+import net.caffeinemc.mods.sodium.client.platform.windows.WindowsFileVersion;
+import net.caffeinemc.mods.sodium.client.platform.windows.api.d3dkmt.D3DKMT;
+import org.jetbrains.annotations.Nullable;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL32C;
+import org.lwjgl.opengl.KHRDebug;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Locale;
 
 public class NvidiaWorkarounds {
     private static final Logger LOGGER = LoggerFactory.getLogger("Sodium-NvidiaWorkarounds");
 
-    public static void install() {
-        LOGGER.warn("Applying workaround: Prevent the NVIDIA OpenGL driver from using broken optimizations (NVIDIA_THREADED_OPTIMIZATIONS)");
+    public static boolean isUsingNvidiaGraphicsCard() {
+        return GraphicsAdapterProbe.getAdapters()
+                .stream()
+                .anyMatch(adapter -> adapter.vendor() == GraphicsAdapterVendor.NVIDIA);
+    }
 
-        try {
-            switch (OsUtils.getOs()) {
-                case WIN -> {
-                    // The NVIDIA drivers rely on parsing the command line arguments to detect Minecraft. If we destroy those,
-                    // then it shouldn't be able to detect us anymore.
-                    WindowsCommandLine.setCommandLine("net.caffeinemc.sodium");
+    // https://github.com/CaffeineMC/sodium/issues/1486
+    // The way which NVIDIA tries to detect the Minecraft process could not be circumvented until fairly recently
+    // So we require that an up-to-date graphics driver is installed so that our workarounds can disable the Threaded
+    // Optimizations driver hack.
+    public static @Nullable WindowsFileVersion findNvidiaDriverMatchingBug1486() {
+        // The Linux driver has two separate branches which have overlapping version numbers, despite also having
+        // different feature sets. As a result, we can't reliably determine which Linux drivers are broken...
+        if (OsUtils.getOs() != OperatingSystem.WIN) {
+            return null;
+        }
 
-                    // Ensures that Minecraft will run on the dedicated GPU, since the drivers can no longer detect it
-                    Kernel32.setEnvironmentVariable("SHIM_MCCOMPAT", "0x800000001");
-                }
-                case LINUX -> {
-                    // Unlike Windows, we don't need to hide ourselves from the driver. We can just request that
-                    // it not use threaded optimizations instead.
-                    Libc.setEnvironmentVariable("__GL_THREADED_OPTIMIZATIONS", "0");
+        for (var adapter : GraphicsAdapterProbe.getAdapters()) {
+            if (adapter.vendor() != GraphicsAdapterVendor.NVIDIA) {
+                continue;
+            }
+
+            if (adapter instanceof D3DKMT.WDDMAdapterInfo wddmAdapterInfo) {
+                var driverVersion = wddmAdapterInfo.openglIcdVersion();
+
+                if (driverVersion.z() == 15) { // Only match 5XX.XX drivers
+                    // Broken in x.y.15.2647 (526.47)
+                    // Fixed in x.y.15.3623 (536.23)
+                    if (driverVersion.w() >= 2647 && driverVersion.w() < 3623) {
+                        return driverVersion;
+                    }
                 }
             }
-        } catch (Throwable t) {
-            LOGGER.error("Failure while applying workarounds", t);
+        }
 
-            LOGGER.error("READ ME! The workarounds for the NVIDIA Graphics Driver did not apply correctly!");
-            LOGGER.error("READ ME! You are very likely going to run into unexplained crashes and severe performance issues!");
-            LOGGER.error("READ ME! Please see this issue for more information: https://github.com/CaffeineMC/sodium-fabric/issues/1816");
+        return null;
+    }
+
+    public static void applyEnvironmentChanges() {
+        // We can't know if the OpenGL context will actually be initialized using the NVIDIA ICD, but we need to
+        // modify the process environment *now* otherwise the driver will initialize with bad settings. For non-NVIDIA
+        // drivers, these workarounds are not likely to cause issues.
+        if (!isUsingNvidiaGraphicsCard()) {
+            return;
+        }
+
+        LOGGER.info("Modifying process environment to apply workarounds for the NVIDIA graphics driver...");
+
+        try {
+            if (OsUtils.getOs() == OperatingSystem.WIN) {
+                applyEnvironmentChanges$Windows();
+            } else if (OsUtils.getOs() == OperatingSystem.LINUX) {
+                applyEnvironmentChanges$Linux();
+            }
+        } catch (Throwable t) {
+            LOGGER.error("Failed to modify the process environment", t);
+            logWarning();
         }
     }
 
-    public static void uninstall() {
-        switch (OsUtils.getOs()) {
-            case WIN -> {
-                WindowsCommandLine.resetCommandLine();
-            }
-            case LINUX -> { }
+
+    private static void applyEnvironmentChanges$Windows() {
+        // The NVIDIA drivers rely on parsing the command line arguments to detect Minecraft. We need to
+        // make sure that it detects the game so that *some* important optimizations are applied. Later,
+        // we will try to enable GL_DEBUG_OUTPUT_SYNCHRONOUS so that "Threaded Optimizations" cannot
+        // be enabled.
+        WindowsCommandLine.setCommandLine("net.caffeinemc.sodium / net.minecraft.client.main.Main /");
+    }
+
+    private static void applyEnvironmentChanges$Linux() {
+        // Unlike Windows, we can just request that it not use threaded optimizations instead.
+        Libc.setEnvironmentVariable("__GL_THREADED_OPTIMIZATIONS", "0");
+    }
+
+    public static void undoEnvironmentChanges() {
+        if (OsUtils.getOs() == OperatingSystem.WIN) {
+            undoEnvironmentChanges$Windows();
         }
+    }
+
+    private static void undoEnvironmentChanges$Windows() {
+        WindowsCommandLine.resetCommandLine();
+    }
+
+    public static void applyContextChanges(GlContextInfo driver) {
+        var normalizedVendorName = driver.vendor()
+                .toLowerCase(Locale.ROOT);
+
+        // The context may not have been initialized with the NVIDIA ICD, even if we think there is an NVIDIA
+        // graphics adapter in use. Because enabling these workarounds have the potential to severely hurt performance
+        // on other drivers, make sure we exit now.
+        if (!normalizedVendorName.startsWith("nvidia")) {
+            return;
+        }
+
+        LOGGER.info("Modifying OpenGL context to apply workarounds for the NVIDIA graphics driver...");
+
+        if (Workarounds.isWorkaroundEnabled(Workarounds.Reference.NVIDIA_THREADED_OPTIMIZATIONS_BROKEN)) {
+            if (OsUtils.getOs() == OperatingSystem.WIN) {
+                applyContextChanges$Windows();
+            }
+        }
+    }
+
+    private static void applyContextChanges$Windows() {
+        // On Windows, the NVIDIA drivers do not have any environment variable to control whether
+        // "Threaded Optimizations" are enabled. But we can enable the "GL_DEBUG_OUTPUT_SYNCHRONOUS" option to
+        // achieve the same effect.
+        var capabilities = GL.getCapabilities();
+
+        if (capabilities.GL_KHR_debug) {
+            LOGGER.info("Enabling GL_DEBUG_OUTPUT_SYNCHRONOUS to force the NVIDIA driver to disable threaded" +
+                    "command submission");
+            GL32C.glEnable(KHRDebug.GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        } else {
+            LOGGER.error("GL_KHR_debug does not appear to be supported, unable to disable threaded " +
+                    "command submission!");
+            logWarning();
+        }
+    }
+
+    private static void logWarning() {
+        LOGGER.error("READ ME!");
+        LOGGER.error("READ ME! The workarounds for the NVIDIA Graphics Driver did not apply correctly!");
+        LOGGER.error("READ ME! You are very likely going to run into unexplained crashes and severe performance issues.");
+        LOGGER.error("READ ME! More information about what went wrong can be found above this message.");
+        LOGGER.error("READ ME!");
+        LOGGER.error("READ ME! Please help us understand why this problem occurred by opening a bug report on our issue tracker:");
+        LOGGER.error("READ ME!   https://github.com/CaffeineMC/sodium/issues");
+        LOGGER.error("READ ME!");
+
     }
 }